dc4f3140c2c4cda309dbec4ee4ccdf4a514514e2
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-04-20 18:32:28 +0530

    Fixed Sequnce Runnables Bugs

--- sequenceRunnable.py
+++ sequenceRunnable.py
@@ -1,12 +1,12 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
-from langchain_core.prompts import ChatMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
 # 1. Creating a prompt template
-prompt = ChatMessagePromptTemplate.from_template(
+prompt = ChatPromptTemplate.from_template(
   "Explain {topic} in simple terms. Explain as if you were explaining it to a 5 year old"
 )
 
@@ -19,6 +19,6 @@ parser = StrOutputParser()
 # 4. Creating runnables and connecting runnables (using | (pipe) operator)
 chain = prompt | model | parser
 
-result = chain.invoke(topic={"Machine Learning"})
+result = chain.invoke({"topic" : "Machine Learning"})
 
 print(result)
